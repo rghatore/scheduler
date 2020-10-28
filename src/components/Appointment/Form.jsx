dc27c87ea2287bb14bb4a6a5a782a5e89a91ab48
Rajdeep Ghatore <r.ghatore@gmail.com>
2020-10-27 22:40:03 -0600

    added Form component and story

---
+++ src/components/Appointment/Form.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import Button from "../Button";
+import InterviewerList from "../InterviewerList";
+
+function Form(props) {
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={props.name}
+            onChange={(event) => props.setName(event.target.value)}
+          /*
+            This must be a controlled component
+          */
+          />
+        </form>
+        <InterviewerList interviewers={props.interviewers} value={props.interviewer} onChange={props.setInterviewer} />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button onClick={props.onCancel} danger>Cancel</Button>
+          <Button onClick={props.onSave} confirm>Save</Button>
+        </section>
+      </section>
+    </main>
+  )
+}
+
+export default Form;
--- stories/index.story.js
+++ stories/index.story.js
@@ -17,6 +17,8 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
+
 
 
 
@@ -155,5 +157,7 @@ storiesOf("Appointment", module)
   .add("Show", () => <Show student="Bruce Wayne" interviewer="Clark Kent" onEdit={action("onEdit")} onDelete={action("onDelete")} />)
   .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
   .add("Status", () => <Status message="Deleting" />)
-  .add("Error", () => <Error message="Could not delete appointment" onClose={action("onClose")} />);
+  .add("Error", () => <Error message="Could not delete appointment" onClose={action("onClose")} />)
+  .add("Create Form", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
+  .add("Edit Form", () => <Form interviewers={interviewers} interviewer={3} name="Bruce Wayne" onSave={action("onSave")} onCancel={action("onCancel")} />);
 
