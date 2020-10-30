ad4402389e2d33a54f99bf6641e28deb0263bacd
Rajdeep Ghatore <r.ghatore@gmail.com>
2020-10-29 18:10:40 -0600

    added axios dependency and retrieved data from scheduler-api server

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,27 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
+const axios = require("axios");
 
 // mock data
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 const appointments = [
   {
@@ -73,12 +74,21 @@ const appointments = [
 export default function Application(props) {
 
   let [day, setDay] = useState("Monday");
+  let [days, setDays] = useState([]);
 
   function interviewSpots (bookings) {
     return bookings.map((booking) => { 
       return <Appointment key={booking.id} {...booking} />
     })
   }
+
+  useEffect(() => {
+    axios.get('/api/days')
+    .then(response => {
+      // console.log(response.data);
+      setDays(response.data);
+    })
+  },[])
     
   return (
     <main className="layout">
