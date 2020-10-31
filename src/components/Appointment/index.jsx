7a473435a3900f5edb8c5388e14b3513dc50c4d1
Rajdeep Ghatore <r.ghatore@gmail.com>
2020-10-30 18:40:54 -0600

    conditionally rendering EMPTY and SHOW components

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -4,6 +4,7 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "../../hooks/useVisualMode";
 
 
 
@@ -11,11 +12,28 @@ function Appointment (props) {
   // console.log('props: ', props)
   // const { student, interviewer } = props.interview;
   // console.log(student, interviewer);
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+
+  // props.interview ? useVisualMode(SHOW) : useVisualMode(EMPTY);
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   const slot = () => {
     // const {student, interviewer} = props.interview;
-    return props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />
+    // return props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />
     // return props.interview ? <Show student interviewer /> : <Empty />
+    if (mode === EMPTY) {
+      return <Empty onAdd={() => console.log("Clicked onAdd")} />
+    } else if (mode === SHOW) {
+      return (
+      <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+      />
+      )
+    }
   }
   
   
