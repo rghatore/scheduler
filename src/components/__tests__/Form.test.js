5e1e38e9faee35c59577dfc92949f2df892f3da7
Rajdeep Ghatore <r.ghatore@gmail.com>
2020-11-03 22:47:26 -0700

    increased test coverage for Form component

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -71,4 +71,19 @@ describe("Form", () => {
     /* onSave is called with the correct arguments */
     expect(onSave).toHaveBeenCalledWith("Clark Kent", null);
   });
+  
+  test("submits the name entered by the user", () => {
+    const onSave = jest.fn();
+    const { getByText, getByPlaceholderText } = render(
+      <Form interviewers={interviewers} onSave={onSave} />
+    );
+  
+    const input = getByPlaceholderText("Enter Student Name");
+  
+    fireEvent.change(input, { target: { value: "Clark Kent" } });
+    fireEvent.click(getByText("Save"));
+  
+    expect(onSave).toHaveBeenCalledTimes(1);
+    expect(onSave).toHaveBeenCalledWith("Clark Kent", null);
+  });
 });
